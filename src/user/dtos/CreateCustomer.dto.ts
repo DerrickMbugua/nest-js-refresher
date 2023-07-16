dfb003cf6829a6e0c03f91e4ea46fb393e106130
“derrick” <derrickmbugua50@gmail.com>
2023-07-16 13:36:58 +0300

    feat: validation

--- src/user/dtos/CreateCustomer.dto.ts
+++ src/user/dtos/CreateCustomer.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsNumberString } from "class-validator";
+
 export class CreateCustomerDto{
+
+  @IsNumberString()
+  @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,31 @@
-import { Body, Controller, Get, HttpException, HttpStatus, NotFoundException, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, NotFoundException, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateCustomerDto } from './dtos/CreateCustomer.dto';
 
 @Controller('user')
 export class UserController {
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService) { }
 
   @Get()
-  getUser(){
+  getUser() {
     return this.userService.findAllUsers();
   }
-  
+
   @Get(':id')
-  getUserById(@Param('id', ParseIntPipe) id: number){
+  getUserById(@Param('id', ParseIntPipe) id: number) {
     const customer = this.userService.findUserById(id);
-    if(!customer){
+    if (!customer) {
       // throw new NotFoundException(`Customer with ID ${id} not found`);
-      throw new HttpException('Customer not found',HttpStatus.BAD_REQUEST);
-    }else{
+      throw new HttpException('Customer not found', HttpStatus.BAD_REQUEST);
+    } else {
       return customer;
     }
   }
 
   @Post()
-  createCustomer(@Body() createCustomerDto: CreateCustomerDto){
+  @UsePipes(ValidationPipe)
+  createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
     this.userService.saveCustomer(createCustomerDto);
     return "Customer created successfully";
   }
